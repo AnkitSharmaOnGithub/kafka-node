bee37427d6a63e1b06cb7a5cb6d1c7525155242f
Ankit Sharma <ankitsharma101299@gmail.com>
2021-12-12 18:15:54 +0530

    Initialized config and added routes.

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const env = require("dotenv");
+env.config();
 
 const app = express();
 
-app.listen(process.env.PORT);
+// Configure routes
+const messagesRoutes = require("./routes/message");
+app.use("/message", messagesRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening on PORT ${process.env.PORT}`);
+});
