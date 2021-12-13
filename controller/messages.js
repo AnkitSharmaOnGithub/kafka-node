ec7f38609a73cb6b63dab71248e3ca0dfab15409
Ankit Sharma <ankitsharma101299@gmail.com>
2021-12-13 08:16:04 +0530

    Add controller method to handle creation of message

--- controller/messages.js
+++ controller/messages.js
@@ -1,3 +1,21 @@
-exports.createMessage = (req, res, next) => {
-  console.log(req.body);
+const producer = require("../kafka/producer");
+
+exports.createMessage = async (req, res, next) => {
+  try {
+    const body = req.body;
+
+    if (body) {
+      const data = [];
+      for (const key in body) {
+        data.push({
+          key: key,
+          value: body[key],
+        });
+      }
+      const result = await producer.produceData("jersey", data, 1);
+      console.log(result);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
