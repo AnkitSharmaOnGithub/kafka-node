42ffe27477270e1769beea462586035972a4d6db
Ankit Sharma <ankitsharma101299@gmail.com>
2021-12-13 08:24:29 +0530

    Add sequelize DB instance to server

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ env.config();
 const messagesRoutes = require("./routes/message");
 app.use("/message", messagesRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is listening on PORT ${process.env.PORT}`);
-});
+// Import in app dependencies
+const sequelize = require("./utils/database");
+
+// Syncing sequelize models with the database
+sequelize
+  .sync()
+  .then((result) => {
+    // Setting up listener to listen on port
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is listening on PORT ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
