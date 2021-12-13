5ccf1522dd87776b0f0801b6b361bb8a420bc1cb
Ankit Sharma <ankitsharma101299@gmail.com>
2021-12-13 08:20:52 +0530

    Add sequelize DB instance

---
+++ utils/database.js
@@ -0,0 +1,20 @@
+const env = require("dotenv");
+env.config();
+
+const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize({
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  dialect: "mysql",
+});
+
+try {
+  await sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
+
+module.exports = sequelize.promise();
