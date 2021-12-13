3415b7c150fe4d826a66696e8d13f21c30e8dc62
Ankit Sharma <ankitsharma101299@gmail.com>
2021-12-13 08:16:50 +0530

    Update broker connection url

--- kafka/kafka-instance.js
+++ kafka/kafka-instance.js
@@ -1,8 +1,11 @@
 const { Kafka, logLevel } = require("kafkajs");
 
+const env = require("dotenv");
+env.config();
+
 const kafka = new Kafka({
   clientId: "palyer-jersey",
-  brokers: [`${process.env.KAFKA_HOST}  ${process.env.KAFKA_PORT}`],
+  brokers: [`${process.env.KAFKA_HOST}:${process.env.KAFKA_PORT}`],
   logLevel: logLevel.ERROR,
 });
 
