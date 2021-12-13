0031effc66f20c06fabd54a27a6ae17a04807304
Ankit Sharma <ankitsharma101299@gmail.com>
2021-12-13 08:21:28 +0530

    Add handler to handle production of message in Kafka

--- kafka/producer.js
+++ kafka/producer.js
@@ -3,9 +3,16 @@ const kafka = require("./kafka-instance");
 const producer = kafka.producer();
 
 exports.produceData = async (topicName, data, partition) => {
-  await producer.send({
-    topic: topicName,
-    messages: data,
-    partition: partition,
-  });
+  console.log(topicName, data, partition);
+  try {
+    await producer.connect({});
+    const sendResult = await producer.send({
+      topic: topicName,
+      messages: data,
+      partition: partition,
+    });
+    return sendResult;
+  } catch (error) {
+    console.log(error);
+  }
 };
